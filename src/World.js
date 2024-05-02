e0fd9a2aec0005edc80e12b473217f9b4d23f301
feesmeji <153968214+feesmeji@users.noreply.github.com>
2024-05-01 20:31:56 -0700

    3 blocks with sky texture (3.2 vid)

--- src/World.js
+++ src/World.js
@@ -21,9 +21,11 @@ var FSHADER_SOURCE = `
   precision mediump float;
   varying vec2 v_UV;
   uniform vec4 u_FragColor;
+  uniform sampler2D u_Sampler0;
   void main() {
     gl_FragColor = u_FragColor;
     gl_FragColor = vec4(v_UV, 1.0, 1.0);   //changes colors of the animal to a neon blue-pink
+    gl_FragColor = texture2D(u_Sampler0, v_UV);
   }` // add a line saying that if I don't want to use a specific texture or not in fragment shader.
 
 //Global Variables
@@ -204,6 +206,53 @@ canvas.addEventListener('mouseup', function(ev) {
 });
 }
 
+function initTextures(gl, n) {
+  var texture = gl.createTexture();   // Create a texture object
+  if (!texture) {
+    console.log('Failed to create the texture object');
+    return false;
+  }
+
+  // Get the storage location of u_Sampler
+  var u_Sampler0 = gl.getUniformLocation(gl.program, 'u_Sampler0');
+  if (!u_Sampler0) {
+    console.log('Failed to get the storage location of u_Sampler0');
+    return false;
+  }
+  var image = new Image();  // Create the image object
+  if (!image) {
+    console.log('Failed to create the image object');
+    return false;
+  }
+  // Register the event handler to be called on loading an image
+  image.onload = function(){ loadTexture(gl, n, texture, u_Sampler0, image); };
+  // Tell the browser to load an image
+  image.src = '../src/sky.jpg';
+
+  return true;
+}
+
+function loadTexture(gl, n, texture, u_Sampler, image) {
+  gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, 1); // Flip the image's y axis
+  // Enable texture unit0
+  gl.activeTexture(gl.TEXTURE0);
+  // Bind the texture object to the target
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+
+  // Set the texture parameters
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+  // Set the texture image
+  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, image);
+  
+  // Set the texture unit 0 to the sampler
+  gl.uniform1i(u_Sampler, 0);
+  
+  gl.clear(gl.COLOR_BUFFER_BIT);   // Clear <canvas>
+
+  gl.drawArrays(gl.TRIANGLE_STRIP, 0, n); // Draw the rectangle
+}
+
+
 function main() {
 
   setupWebGL();
@@ -229,6 +278,8 @@ function main() {
 
   //renderAllShapes();
   requestAnimationFrame(tick);
+
+  initTextures(gl,0);
 }
 
 //var g_shapesList = [];
